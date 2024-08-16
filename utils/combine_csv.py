075e4e7e379c4c91751a6922f5b48b1829ab85fa
Yifan Luo <jeffluoyifan@gmail.com>
2024-08-16 11:44:13 +1000

    Update combine_csv.py

--- utils/combine_csv.py
+++ utils/combine_csv.py
@@ -1,20 +1,20 @@
 import os
 import re
 
-import numpy as np
+import pandas as pd
 
 
 def list_files(directory):
     file_paths = []
     for root, _, files in os.walk(directory):
         for file in files:
-            if re.match(r".*textpos.*.csv", file):
+            if re.match(r"coanzse_textpos_distributable_07092023_[0-9]+.csv", file):
                 file_path = os.path.join(root, file)
                 file_paths.append(file_path)
     return file_paths
 
 
-def combine(csv_paths, save_path)
+def combine(csv_paths, save_path):
     all_csv = pd.DataFrame([])
 
     for file_path in csv_paths:
@@ -22,8 +22,9 @@ def combine(csv_paths, save_path)
         df = pd.read_csv(csv_paths[0], escapechar=',', header=None)
         all_csv = pd.concat((all_csv, df))
 
+    all_csv = all_csv.drop_duplicates()
     print(f"Saving into {save_path + '/' + 'textpos.csv'}")
-    all_csv.to_csv(save_path + '/' + 'textpos.csv', index=False, header=None, encoding='utf-8')
+    all_csv.to_csv(save_path + '/' + 'textpos.csv', index=False, header=["paragraph"], encoding='utf-8')
     
 
 
